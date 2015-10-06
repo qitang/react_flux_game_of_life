6edbadfa871a4c1803b6615b190cbeef0edb5ba8
qitang <qtang@acupay.com>
2015-10-06 16:13:46 -0400

    add express into package

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@
 
 var express = require('express');
 var app     = express();
-var port    =   process.env.PORT || 8080;
+var port    =   process.env.PORT || 3000;
 var path = require('path');
 
 var bodyParser = require('body-parser');
